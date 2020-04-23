4b8a31e64dfc2676f14146f81fa57079c8e185e6
Ash <ash@drashsmith.com>
2020-04-23 09:26:58 +0100

    Skinned the forms to go better with the AdminLTE interface

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm, ImageField
+from django.forms import ModelForm, ImageField, TextInput, Textarea, Select
 from viewer.models import Location, Event
 from django.db.models import Sum, Count
 import datetime, pytz
@@ -7,11 +7,30 @@ class LocationForm(ModelForm):
     class Meta:
         model = Location
         fields = ['uid', 'label', 'full_label', 'address', 'phone', 'description', 'lat', 'lon', 'url', 'wikipedia', 'image', 'creation_time', 'destruction_time']
+        widgets = {
+            'uid': TextInput(attrs={'class': 'form-control'}),
+            'label': TextInput(attrs={'class': 'form-control'}),
+            'full_label': TextInput(attrs={'class': 'form-control'}),
+            'phone': TextInput(attrs={'class': 'form-control'}),
+            'lat': TextInput(attrs={'class': 'form-control'}),
+            'lon': TextInput(attrs={'class': 'form-control'}),
+            'url': TextInput(attrs={'class': 'form-control'}),
+            'wikipedia': TextInput(attrs={'class': 'form-control'}),
+            'address': Textarea(attrs={'class': 'form-control'}),
+            'description': Textarea(attrs={'class': 'form-control'}),
+            'creation_time': TextInput(attrs={'class': 'form-control'}),
+            'destruction_time': TextInput(attrs={'class': 'form-control'}),
+        }
 
 class QuickEventForm(ModelForm):
     class Meta:
         model = Event
         fields = ['caption', 'type', 'description']
+        widgets = {
+            'caption': TextInput(attrs={'class': 'form-control'}),
+            'type': TextInput(attrs={'class': 'form-control'}),
+            'description': Textarea(attrs={'class': 'form-control'}),
+        }
 
 class EventForm(ModelForm):
     def __init__(self, *args, **kwargs):
@@ -21,3 +40,11 @@ class EventForm(ModelForm):
     class Meta:
         model = Event
         fields = ['caption', 'type', 'description', 'start_time', 'end_time', 'location']
+        widgets = {
+            'caption': TextInput(attrs={'class': 'form-control'}),
+            'type': TextInput(attrs={'class': 'form-control'}),
+            'location': Select(attrs={'class': 'form-control'}),
+            'description': Textarea(attrs={'class': 'form-control'}),
+            'start_time': TextInput(attrs={'class': 'form-control'}),
+            'end_time': TextInput(attrs={'class': 'form-control'}),
+        }
