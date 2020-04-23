8ec86d62e66a8e602569359a75d5a3bab0cc16ba
Ash <ash@drashsmith.com>
2020-04-23 17:20:30 +0100

    Fixed a bug in the geoline generator - it now respects timezones

--- functions.py
+++ functions.py
@@ -6,7 +6,7 @@ from geopy import distance
 
 def getgeoline(dts, dte, address='127.0.0.1:8000'):
 
-    id = dts.strftime("%Y%m%d%H%M%S") + dte.strftime("%Y%m%d%H%M%S")
+    id = dts.astimezone(pytz.UTC).strftime("%Y%m%d%H%M%S") + dte.astimezone(pytz.UTC).strftime("%Y%m%d%H%M%S")
     url = "http://" + address + "/location-manager/route/" + id + "?format=json"
     data = {}
     with urllib.request.urlopen(url) as h:
