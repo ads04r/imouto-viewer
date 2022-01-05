5169fe5c82521cd59dcf1d25e214bdd449904351
Ash <ash@drashsmith.com>
2022-01-05 00:19:37 +0000

    import_photos management command now optionally accepts a timezone as an argument

--- management/commands/import_photos.py
+++ management/commands/import_photos.py
@@ -11,10 +11,19 @@ class Command(BaseCommand):
 	def add_arguments(self, parser):
 
 		parser.add_argument("-i", "--input", action="store", dest="photo_path", default="", help="A path to search for photos.")
+		parser.add_argument("-z", "--timezone", action="store", dest="tz_info", default="", help="The timezone of the photos, useful for when none is specified in the EXIF data.")
 
 	def handle(self, *args, **kwargs):
 
 		path = kwargs['photo_path']
+		tz_string = kwargs['tz_info']
+		if tz_string == '':
+			if settings.TIME_ZONE:
+				tz = pytz.timezone(settings.TIME_ZONE)
+			else:
+				tz = pytz.UTC
+		else:
+			tz = pytz.timezone(tz_string)
 		if len(path) == 0:
 			sys.stderr.write(self.style.ERROR("Invalid path to images; must be specified using the --input argument.\n"))
 			sys.exit(1)
@@ -22,5 +31,5 @@ class Command(BaseCommand):
 			sys.stderr.write(self.style.ERROR("Path '" + path + "' not found.\n"))
 			sys.exit(1)
 
-		ret = import_photo_directory(path, pytz.timezone(settings.TIME_ZONE))
+		ret = import_photo_directory(path, tz)
 		sys.stderr.write(self.style.SUCCESS(str(len(ret)) + " new photo(s) found.\n"))
