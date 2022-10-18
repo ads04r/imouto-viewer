ads04r/imouto-viewer

commit 6afdeabcdd815ccfb58d89348e17501140196356

Closes #37

viewer/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.files import File
-from django.db.models import Count
+from django.db.models import Count, Avg, Transform, Field, IntegerField, F, ExpressionWrapper
+from django.db.models.fields import DurationField
 from django.conf import settings
 from django.utils.html import strip_tags
 from colorfield.fields import ColorField
@@ -13,6 +14,14 @@ from staticmap import StaticMap, Line
 from viewer.staticcharts import generate_pie_chart, generate_donut_chart
 from xml.dom import minidom
 import random, datetime, pytz, json, markdown, re, os, urllib.request
+
+@Field.register_lookup
+class WeekdayLookup(Transform):
+	lookup_name = 'wd'
+	function = 'DAYOFWEEK'
+	@property
+	def output_field(self):
+		return IntegerField()
 
 def user_thumbnail_upload_location(instance, filename):
 	return 'people/' + str(instance.pk) + '/' + filename
@@ -177,6 +186,23 @@ class Location(models.Model):
 			if dt < self.creation_time:
 				return False
 		return True
+	def tags(self):
+		return EventTag.objects.filter(events__location=self).distinct().exclude(id='')
+	def longest_event(self):
+		duration = ExpressionWrapper(F('end_time') - F('start_time'), output_field=DurationField())
+		return Event.objects.filter(location=self).exclude(type='life_event').annotate(duration=duration).order_by('-duration')[0].duration
+	def average_event(self):
+		duration = ExpressionWrapper(F('end_time') - F('start_time'), output_field=DurationField())
+		return Event.objects.filter(location=self).exclude(type='life_event').annotate(duration=duration).aggregate(av=Avg('duration'))['av']
+	def weekdays(self):
+		ret = {'days': [], 'values': []}
+		d = ['', 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+		for item in Event.objects.filter(location=self).exclude(type='life_event').values('start_time__wd').annotate(ct=Count('start_time__wd')).order_by('start_time__wd'):
+			id = item['start_time__wd']
+			ret['days'].append(d[id])
+			ret['values'].append(item['ct'])
+		ret['json'] = json.dumps(ret['days'])
+		return ret
 	def __str__(self):
 		label = self.label
 		if self.full_label != '':
@@ -680,6 +706,7 @@ class Event(models.Model):
 			if 'distance' in geo['properties']:
 				return (float(int((geo['properties']['distance'] / 1.609) * 100)) / 100)
 		return 0
+	@property
 	def length(self):
 		return((self.end_time - self.start_time).total_seconds())
 	def length_string(self, value=0):

viewer/templates/viewer/imouto.js
@@ -985,6 +985,7 @@ function placeScreen(id)
     $(".content-wrapper").load("./places/" + id + ".html", function(response, status, xhr){
         if(status == 'error') { errorPage(xhr); return false; }
         makeMap();
+        initialiseGraphics();
     });
 }
 

viewer/templates/viewer/place.html
@@ -101,6 +101,26 @@
                         <div id="map{{ data.pk }}" class="eventmap" data-geojson="{{ data.geo }}"></div>
                     </div>
                 </div>
+
+                <div class="box box-primary">
+                    <div class="box-body">
+			<canvas class="donut-chart" style="height: 160px" data-data="{{ data.weekdays.values|escape }}" data-labels="{{ data.weekdays.json|escape }}"></canvas>
+                        <ul class="list-group list-group-unbordered">
+                          <li class="list-group-item"><b>Average visit</b><span class="pull-right">{{ data.average_event }}</span></li>
+                          <li class="list-group-item"><b>Longest visit</b><span class="pull-right">{{ data.longest_event }}</span></li>
+                        </ul>
+{% if data.tags.count > 0 %}
+
+                	<div>
+
+                        {% for tag in data.tags.all %}<a href="#tag_{{ tag.id|safe }}"><span style="background-color: {{ tag.colour }};" class="badge">{{ tag.id }}</span></a> {% endfor %}
+
+	                </div>
+
+{% endif %}
+                    </div>
+                </div>
+
             </div>
 
         </div>

commit 27fca420178d9c1e7c7e85254fb94754ff4e5757

Fixed a bug introduced in https://github.com/ads04r/imouto-viewer/commit/6afdeabcdd815ccfb58d89348e17501140196356

viewer/models.py
@@ -800,7 +800,7 @@ class Event(models.Model):
 		elev_max = -99999.99
 		elev_min = 99999.99
 		sleep = []
-		if self.length() > 86400:
+		if self.length > 86400:
 			eventsearch = DataReading.objects.filter(end_time__gte=self.start_time, start_time__lte=self.end_time).exclude(type='heart-rate').exclude(type='cadence')
 		else:
 			eventsearch = DataReading.objects.filter(end_time__gte=self.start_time, start_time__lte=self.end_time)
@@ -866,7 +866,7 @@ class Event(models.Model):
 			ret['heartmax'] = int(heart_max)
 			ret['heartavgprc'] = int(((heart_total / heart_count) / self.max_heart_rate()) * 100)
 			ret['heartmaxprc'] = int((heart_max / self.max_heart_rate()) * 100)
-			ret['heartzonetime'] = [int(self.length() - (heart_zone + heart_zone_2)), int(heart_zone), int(heart_zone_2)]
+			ret['heartzonetime'] = [int(self.length - (heart_zone + heart_zone_2)), int(heart_zone), int(heart_zone_2)]
 			ret['heartoptimaltime'] = self.length_string(ret['heartzonetime'][1])
 			ret['efficiency'] = int((float(ret['heartzonetime'][1]) / float(ret['heartzonetime'][0] + ret['heartzonetime'][1] + ret['heartzonetime'][2])) * 100.0)
 			ret['heart'] = ','.join(heart_csv)