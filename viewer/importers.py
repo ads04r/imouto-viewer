ads04r/imouto-viewer

commit 6dac2fae4c64c07f9f2fdaa2388519117417d00b

Removing more references to tzlocal

viewer/importers.py
@@ -3,7 +3,6 @@ import datetime, pytz, pymysql, math
 from django.conf import settings
 from xml.dom import minidom
 from fitparse import FitFile
-from tzlocal import get_localzone
 from xml.dom import minidom
 from requests import request
 from requests.auth import HTTPBasicAuth, HTTPDigestAuth

viewer/management/commands/import_fit.py
@@ -3,7 +3,6 @@ from django.core.cache import cache
 from django.conf import settings
 from viewer.models import DataReading, Event
 from io import StringIO
-from dateutil.tz import tzlocal
 import os, sys, datetime, shutil, sqlite3, pytz, csv, xmltodict, json
 from viewer.models import RemoteInteraction
 from viewer.importers import import_fit

viewer/management/commands/import_sms.py
@@ -3,7 +3,6 @@ from django.core.cache import cache
 from django.conf import settings
 from viewer.models import DataReading, Event
 from io import StringIO
-from dateutil.tz import tzlocal
 import os, sys, datetime, shutil, sqlite3, pytz, csv, xmltodict, json
 from viewer.models import RemoteInteraction
 
@@ -189,7 +188,7 @@ class Command(BaseCommand):
 				continue
 			if message['1020'] != 'PIT_MESSAGE_INBOX':
 				continue
-			dt = datetime.datetime.strptime(message['1041'], '%Y-%m-%dT%H:%M').replace(tzinfo=tzlocal())
+			dt = datetime.datetime.strptime(message['1041'], '%Y-%m-%dT%H:%M').replace(tzinfo=pytz.timezone(settings.TIME_ZONE))
 
 			phone_number = self.format_number(message['1080'])
 			try:
@@ -209,7 +208,7 @@ class Command(BaseCommand):
 				continue
 			if message['1021'] != 'PIT_MESSAGE_OUTBOX':
 				continue
-			dt = datetime.datetime.strptime(message['1041'], '%Y-%m-%dT%H:%M').replace(tzinfo=tzlocal())
+			dt = datetime.datetime.strptime(message['1041'], '%Y-%m-%dT%H:%M').replace(tzinfo=pytz.timezone(settings.TIME_ZONE))
 			try:
 				msg = RemoteInteraction.objects.get(type='sms', time=dt, address=message['1081'], incoming=True)
 			except:

viewer/views.py
@@ -10,7 +10,7 @@ from rest_framework.exceptions import MethodNotAllowed
 from background_task.models import Task
 from haystack.query import SearchQuerySet
 from viewer.tasks import generate_photo_collages
-import datetime, pytz, dateutil.parser, json, tzlocal, requests
+import datetime, pytz, dateutil.parser, json, requests
 
 from .tasks import *
 from .models import *
@@ -191,7 +191,7 @@ def timelineitem(request, ds):
 	dsyear = int(ds[0:4])
 	dsmonth = int(ds[4:6])
 	dsday = int(ds[6:])
-	dtq = datetime.datetime(dsyear, dsmonth, dsday, 0, 0, 0, tzinfo=tzlocal.get_localzone())
+	dtq = datetime.datetime(dsyear, dsmonth, dsday, 0, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
 	events = get_timeline_events(dtq)
 
 	dtq = events[0].start_time
@@ -625,7 +625,7 @@ def eventjson(request):
 	dse = request.GET.get('end', '')
 	dts = dateutil.parser.parse(dss)
 	dte = dateutil.parser.parse(dse)
-	tz = tzlocal.get_localzone()
+	tz = pytz.timezone(settings.TIME_ZONE)
 	ret = []
 	for event in Event.objects.filter(end_time__gte=dts).filter(start_time__lte=dte):
 		item = {}

commit 57eeb487f729d049741238a6e5b99f825d2bd2f7

Fixed some bugs introduced by https://github.com/ads04r/imouto-viewer/commit/6dac2fae4c64c07f9f2fdaa2388519117417d00b

viewer/importers.py
@@ -34,7 +34,7 @@ def export_monica_calls(from_date=None):
 
 	dtsd = from_date
 	now = datetime.datetime.now()
-	tz = get_localzone()
+	tz = pytz.timezone(settings.TIME_ZONE)
 	ret = []
 	if from_date is None:
 		dtsd = get_last_monica_call() + datetime.timedelta(days=1)
@@ -72,7 +72,7 @@ def export_monica_events(from_date=None):
 
 	dtsd = from_date
 	now = datetime.datetime.now()
-	tz = get_localzone()
+	tz = pytz.timezone(settings.TIME_ZONE)
 	ret = []
 	if from_date is None:
 		dtsd = get_last_monica_activity() + datetime.timedelta(days=1)
@@ -281,7 +281,6 @@ def import_fit(parseable_fit_input):
 
 	data = []
 	fit = FitFile(parseable_fit_input)
-	tz = get_localzone()
 	tz = pytz.UTC
 	for record in fit.get_messages('record'):
 		item = {}