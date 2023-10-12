79edd0fcc73a1ed3c30422d994dadbad71738c29
Ash <ash@drashsmith.com>
2023-10-12 23:10:51 +0100

    Added a function for querying a location by time from the location manager

--- viewer/functions/location_manager.py
+++ viewer/functions/location_manager.py
@@ -2,6 +2,24 @@ import datetime, pytz, json, re, sys, os, requests
 from django.conf import settings
 from dateutil import parser
 
+def get_logged_position(dt):
+	"""
+	Given a date, returns the point that the user was near at the time specified.
+
+	:param date: A datetime.
+	:return: A tuple (lat, lon) indicating the user's position
+	:rtype: tuple(float)
+
+	"""
+	ds = dt.astimezone(pytz.UTC).strftime("%Y%m%d%H%M%S")
+	url = settings.LOCATION_MANAGER_URL + '/position/' + ds
+	r = requests.get(url)
+	data = json.loads(r.text)
+	if(('lat' in data) & ('lon' in data)):
+		return (data['lat'], data['lon'])
+
+	return (None, None)
+
 def get_possible_location_events(date, lat, lon):
 	"""
 	Given a date and a query point, returns a list of times the user was near the query point on the day specified.
