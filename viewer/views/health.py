ads04r/imouto-viewer

commit d887d28a39511dc952fbd011e7ad12c8ce6579f3

Closes #149

viewer/views/health.py
@@ -42,7 +42,7 @@ def health(request, pageid):
 	if pageid == 'heart':
 		return render(request, 'viewer/pages/health_heart.html', context)
 	if pageid == 'sleep':
-		dt = datetime.datetime.now().replace(hour=0, minute=0, second=0, tzinfo=pytz.UTC)
+		dt = pytz.utc.localize(datetime.datetime.now().replace(hour=0, minute=0, second=0))
 		expression = F('end_time') - F('start_time')
 		wrapped_expression = ExpressionWrapper(expression, DurationField())
 		context['data'] = {'stats': [], 'sleeps': []}
@@ -83,7 +83,7 @@ def health(request, pageid):
 	if pageid == 'blood':
 		if request.method == 'POST':
 			ret = json.loads(request.body)
-			dt = datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=pytz.UTC)
+			dt = pytz.utc.localize(datetime.datetime.now(datetime.timezone.utc))
 			datapoint = DataReading(type='bp_sys', start_time=dt, end_time=dt, value=ret['bp_sys_val'])
 			datapoint.save()
 			datapoint = DataReading(type='bp_dia', start_time=dt, end_time=dt, value=ret['bp_dia_val'])
@@ -95,7 +95,7 @@ def health(request, pageid):
 	if pageid == 'weight':
 		context['data'] = health_data(['weight', 'fat', 'muscle', 'water'])
 		context['graphs'] = {}
-		dt = datetime.datetime.now().replace(hour=0, minute=0, second=0, tzinfo=pytz.UTC)
+		dt = pytz.utc.localize(datetime.datetime.now()).replace(hour=0, minute=0, second=0)
 		for i in [{'label': 'week', 'dt': (dt - datetime.timedelta(days=7))}, {'label': 'month', 'dt': (dt - datetime.timedelta(days=28))}, {'label': 'year', 'dt': (dt - datetime.timedelta(days=365))}]:
 			item = []
 			min_dt = i['dt'].timestamp()
@@ -109,7 +109,7 @@ def health(request, pageid):
 	if pageid == 'mentalhealth':
 		if request.method == 'POST':
 			ret = json.loads(request.body)
-			dt = datetime.datetime.now(datetime.timezone.utc).replace(tzinfo=pytz.UTC)
+			dt = pytz.utc.localize(datetime.datetime.now(datetime.timezone.utc))
 			datapoint = DataReading(type='hads-a', start_time=dt, end_time=dt, value=ret['anxiety'])
 			datapoint.save()
 			datapoint = DataReading(type='hads-d', start_time=dt, end_time=dt, value=ret['depression'])
@@ -142,7 +142,7 @@ def mood(request):
 		m = 5
 	if m == 0:
 		raise Http404()
-	dt = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC).astimezone(pytz.timezone(settings.TIME_ZONE))
+	dt = pytz.utc.localize(datetime.datetime.utcnow()).astimezone(pytz.timezone(settings.TIME_ZONE))
 	entry = DataReading(start_time=dt, end_time=dt, type='mood', value=m)
 	entry.save()
 	ret = {"date": dt, "mood": m, "created": entry.pk}

viewer/functions/health.py
@@ -15,7 +15,7 @@ def get_heart_history(days):
 	:return: A list of two-value lists consisting of a time offset and a heart rate in beats per minute.
 	:rtype: list
 	"""
-	dte = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC).astimezone(pytz.timezone(settings.TIME_ZONE)).replace(hour=0, minute=0, second=0)
+	dte = pytz.utc.localize(datetime.datetime.utcnow()).astimezone(pytz.timezone(settings.TIME_ZONE)).replace(hour=0, minute=0, second=0)
 	dts = dte - datetime.timedelta(days=days)
 	data = DataReading.objects.filter(start_time__gte=dts, type='heart-rate').order_by('start_time')
 	ret = []
@@ -40,7 +40,7 @@ def get_heart_graph(dt):
 	if not(ret is None):
 		return ret
 
-	dts = datetime.datetime(dt.year, dt.month, dt.day, 4, 0, 0, tzinfo=pytz.timezone(settings.TIME_ZONE))
+	dts = pytz.timezone(settings.TIME_ZONE).localize(datetime.datetime(dt.year, dt.month, dt.day, 4, 0, 0))
 	dte = dts + datetime.timedelta(days=1)
 	last = None
 	values = {}

viewer/health.py
@@ -4,10 +4,10 @@ from django.conf import settings
 
 def max_heart_rate(self, at_time=None):
 	if at_time is None:
-		dt = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+		dt = pytz.utc.localize(datetime.datetime.utcnow())
 	else:
 		dt = at_time
-	age = int(((dt - datetime.datetime(settings.USER_DATE_OF_BIRTH.year, settings.USER_DATE_OF_BIRTH.month, settings.USER_DATE_OF_BIRTH.day, 0, 0, 0, tzinfo=dt.tzinfo)).days) / 365.25)
+	age = int(((dt - dt.tzinfo.localize(datetime.datetime(settings.USER_DATE_OF_BIRTH.year, settings.USER_DATE_OF_BIRTH.month, settings.USER_DATE_OF_BIRTH.day, 0, 0, 0))).days) / 365.25)
 	return (220 - age)
 
 def parse_sleep(sleep, timezone=None):
@@ -17,8 +17,8 @@ def parse_sleep(sleep, timezone=None):
 	else:
 		tz = timezone
 
-	time_from = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
-	time_to = datetime.datetime(1970, 1, 1, 0, 0, 0, tzinfo=pytz.UTC)
+	time_from = pytz.utc.localize(datetime.datetime.utcnow())
+	time_to = pytz.utc.localize(datetime.datetime(1970, 1, 1, 0, 0, 0))
 	ret = []
 	for item in sleep:
 		if item.start_time < time_from:

viewer/forms.py
@@ -45,7 +45,7 @@ class QuickEventForm(ModelForm):
 class EventForm(ModelForm):
 	def __init__(self, *args, **kwargs):
 		super().__init__(*args, **kwargs)
-		now = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+		now = pytz.utc.localize(datetime.datetime.utcnow())
 		self.fields['location'].queryset = Location.objects.exclude(creation_time__gt=now).exclude(destruction_time__lt=now).annotate(num_events=Count('events')).order_by('-num_events')
 	class Meta:
 		model = Event
@@ -60,9 +60,6 @@ class EventForm(ModelForm):
 		}
 
 class LifePeriodForm(ModelForm):
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		now = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
 	class Meta:
 		model = LifePeriod
 		fields = ['caption', 'type', 'description', 'start_time', 'end_time']

viewer/functions/locations.py
@@ -26,7 +26,7 @@ def nearest_location(lat, lon):
 	:rtype: Location
 
 	"""
-	now = datetime.datetime.now().replace(tzinfo=pytz.UTC)
+	now = pytz.utc.localize(datetime.datetime.utcnow())
 	dist = 99999.9
 	ret = None
 	check = (lat, lon)
@@ -52,7 +52,7 @@ def create_location_events(minlength=300):
 	except:
 		home_id = -1
 	dts = Event.objects.filter(type='loc_prox').exclude(location__id=home_id).order_by('-start_time')[0].start_time
-	dte = datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, tzinfo=pytz.UTC) + datetime.timedelta(days=1)
+	dte = pytz.utc.localize(datetime.datetime.utcnow()).replace(hour=0, minute=0, second=0) + datetime.timedelta(days=1)
 	ret = []
 	while dts < dte:
 		url = settings.LOCATION_MANAGER_URL + '/event/' + dts.strftime("%Y-%m-%d")

viewer/functions/monica.py
@@ -33,7 +33,7 @@ def get_people_without_monica_hash(since=None):
 		if Event.objects.count() > 0:
 			dt = Event.objects.order_by('start_time')[0].start_time
 		else:
-			dt = datetime.datetime(1970, 1, 1, 0, 0, 0, tzinfo=pytz.UTC)
+			dt = pytz.utc.localize(datetime.datetime(1970, 1, 1, 0, 0, 0))
 	else:
 		dt = since
 	people = Person.objects.filter(personevent__event__start_time__gte=dt).distinct()
@@ -301,7 +301,7 @@ def get_last_monica_activity():
 
 def get_last_monica_call():
 
-	dt = datetime.datetime(1970, 1, 1, 0, 0, 0, tzinfo=pytz.UTC)
+	dt = pytz.utc.localize(datetime.datetime(1970, 1, 1, 0, 0, 0))
 	for act in get_monica_call_data():
 		if not('called_at' in act):
 			continue

viewer/functions/photos.py
@@ -6,7 +6,7 @@ def locate_photos_by_exif(since=None, reassign=False):
 
 	ret = 0
 	if since is None:
-		datecutoff = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC) - datetime.timedelta(days=60)
+		datecutoff = pytz.utc.localize(datetime.datetime.utcnow()) - datetime.timedelta(days=60)
 	else:
 		datecutoff = since
 	if reassign:
@@ -32,7 +32,7 @@ def locate_photos_by_exif(since=None, reassign=False):
 def bubble_photo_locations(since=None, loc_id=None, reassign=False):
 
 	if since is None:
-		datecutoff = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC) - datetime.timedelta(days=60)
+		datecutoff = pytz.utc.localize(datetime.datetime.utcnow()) - datetime.timedelta(days=60)
 	else:
 		datecutoff = since
 	if loc_id is None:

viewer/functions/utils.py
@@ -17,7 +17,7 @@ def __display_timeline_event(event):
 				if event.photos().count() == 0:
 					return False
 		if event.type == 'journey':
-			ddt = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC) - datetime.timedelta(days=7)
+			ddt = pytz.utc.localize(datetime.datetime.utcnow()) - datetime.timedelta(days=7)
 			if event.end_time < ddt:
 				if event.people.all().count() == 0:
 					if event.photos().count() == 0:
@@ -38,8 +38,8 @@ def generate_life_grid(start_date, weeks):
 			if len(year) > 0:
 				life.append(year)
 				year = []
-		dtts = datetime.datetime(dts.year, dts.month, dts.day, 0, 0, 0, tzinfo=pytz.UTC)
-		dtte = datetime.datetime(dte.year, dte.month, dte.day, 23, 59, 59, tzinfo=pytz.UTC)
+		dtts = pytz.utc.localize(datetime.datetime(dts.year, dts.month, dts.day, 0, 0, 0))
+		dtte = pytz.utc.localize(datetime.datetime(dte.year, dte.month, dte.day, 23, 59, 59))
 		item = {'start_time': dts, 'end_time': dte, 'events': Event.objects.filter(type='life_event', start_time__lte=dtte, end_time__gte=dtts), 'periods': LifePeriod.objects.filter(start_time__lte=dtte, end_time__gte=dtts)}
 		year.append(item)
 	if len(year) > 0:
@@ -109,7 +109,7 @@ def generate_onthisday():
 
 		offset = datetime.timedelta(hours=4)
 
-		dt = datetime.datetime.now().replace(tzinfo=pytz.timezone(settings.TIME_ZONE))
+		dt = pytz.utc.localize(datetime.datetime.utcnow()).astimezone(pytz.timezone(settings.TIME_ZONE))
 		year = dt.year - i
 		dt = dt.replace(year=year)
 		dts = dt.replace(hour=0, minute=0, second=0) + offset
@@ -270,7 +270,7 @@ def generate_dashboard():
 	total_steps = 0
 	for i in range(0, 7):
 		dtbase = last_record - datetime.timedelta(days=(7 - i))
-		dt = datetime.datetime(dtbase.year, dtbase.month, dtbase.day, 0, 0, 0, tzinfo=dtbase.tzinfo)
+		dt = dtbase.tzinfo.localize(datetime.datetime(dtbase.year, dtbase.month, dtbase.day, 0, 0, 0))
 		obj = DataReading.objects.filter(type='step-count').filter(start_time__gte=dt, end_time__lt=(dt + datetime.timedelta(days=1))).aggregate(Sum('value'))
 		try:
 			steps = int(obj['value__sum'])
@@ -284,7 +284,9 @@ def generate_dashboard():
 		stepdata.append(item)
 	stats['steps'] = total_steps
 	walk_dist = 0.0
-	dt = (last_record - datetime.timedelta(days=7)).replace(hour=0, minute=0, second=0, tzinfo=pytz.UTC)
+	dt = (last_record - datetime.timedelta(days=7)).replace(hour=0, minute=0, second=0)
+	if pytz.tzinfo is None:
+		dt = pytz.utc.localize(dt)
 	for walk in DataReading.objects.filter(start_time__gte=dt, type='pebble-app-activity', value='5'):
 		try:
 			ev = Event.objects.get(start_time=walk.start_time, end_time=walk.end_time, type='journey')
@@ -319,7 +321,7 @@ def generate_dashboard():
 	sleepdata = []
 	for i in range(0, 7):
 		dtbase = last_record - datetime.timedelta(days=(7 - i))
-		dt = datetime.datetime(dtbase.year, dtbase.month, dtbase.day, 16, 0, 0, tzinfo=dtbase.tzinfo)
+		dt = dtbase.tzinfo.localize(datetime.datetime(dtbase.year, dtbase.month, dtbase.day, 16, 0, 0))
 		total_sleep = 0
 		deep_sleep = 0
 		obj = DataReading.objects.filter(type='sleep').filter(value=1).filter(start_time__gte=(dt - datetime.timedelta(days=1))).filter(end_time__lt=dt)

commit a2faa446fda8b6612156d6b92cf384a360df876a

Fixes an issue with the mental health page introduced in d887d28a39511dc952fbd011e7ad12c8ce6579f3

viewer/views/health.py
@@ -109,7 +109,7 @@ def health(request, pageid):
 	if pageid == 'mentalhealth':
 		if request.method == 'POST':
 			ret = json.loads(request.body)
-			dt = pytz.utc.localize(datetime.datetime.now(datetime.timezone.utc))
+			dt = pytz.utc.localize(datetime.datetime.utcnow())
 			datapoint = DataReading(type='hads-a', start_time=dt, end_time=dt, value=ret['anxiety'])
 			datapoint.save()
 			datapoint = DataReading(type='hads-d', start_time=dt, end_time=dt, value=ret['depression'])