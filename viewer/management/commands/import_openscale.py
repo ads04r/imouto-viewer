93f9c91d3717e9fe17f8aa58a2104dec3a488ad9
Ash <ash@drashsmith.com>
2023-11-16 17:07:49 +0000

    Fixed a bug introduced by my previous code re-arranging

--- viewer/management/commands/import_openscale.py
+++ viewer/management/commands/import_openscale.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.core.cache import cache
 from django.conf import settings
-from viewer.importers import import_openscale
+from viewer.importers.openscale import import_openscale
 import os, sys
 
 class Command(BaseCommand):
