2a25fa13869ac90f950697380feec3b8b786fe89
Ash <ash@drashsmith.com>
2024-01-30 23:09:19 +0000

    Improved the UI of the admin panel slightly

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 from .models import *
 
+class PersonAdmin(admin.ModelAdmin):
+	list_display = ['full_name', 'nickname', 'significant']
+	ordering = ['-significant', 'family_name', 'given_name', 'nickname']
+
+class LocationAdmin(admin.ModelAdmin):
+	list_display = ['full_label', 'address', 'exists', 'city', 'country']
+
 admin.site.register(Event)
-admin.site.register(Person)
-admin.site.register(Location)
-admin.site.register(Photo)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Location, LocationAdmin)
 admin.site.register(DataReading)
 admin.site.register(RemoteInteraction)
 admin.site.register(EventTag)
+admin.site.register(LocationCountry)
+admin.site.register(LocationCity)
 
