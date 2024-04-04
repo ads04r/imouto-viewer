7a2cbcf847a8bc4353a906145f032b70e0a9fb12
Ash <ash@drashsmith.com>
2024-04-04 22:55:15 +0100

    Added RDF views - this should have been done several commits ago

---
+++ viewer/views/rdf.py
@@ -0,0 +1,38 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import get_object_or_404
+from viewer.models import Event, Year
+from viewer.functions.rdf import rdf_serialize
+
+def event_rdf(request, eid):
+	data = get_object_or_404(Event, id=eid)
+	rdf = rdf_serialize(data, format='pretty-xml')
+	if rdf:
+		response = HttpResponse(rdf, content_type='application/rdf+xml')
+		return response
+	raise Http404()
+
+def event_ttl(request, eid):
+	data = get_object_or_404(Event, id=eid)
+	rdf = rdf_serialize(data, format='turtle')
+	if rdf:
+		response = HttpResponse(rdf, content_type='application/turtle')
+		return response
+	raise Http404()
+
+def year_rdf(request, ds):
+	data = get_object_or_404(Year, year=ds)
+	rdf = rdf_serialize(data, format='pretty-xml')
+	if rdf:
+		response = HttpResponse(rdf, content_type='application/rdf+xml')
+		return response
+	raise Http404()
+
+def year_ttl(request, ds):
+	data = get_object_or_404(Year, year=ds)
+	rdf = rdf_serialize(data, format='turtle')
+	if rdf:
+		response = HttpResponse(rdf, content_type='application/turtle')
+		return response
+	raise Http404()
+
+
