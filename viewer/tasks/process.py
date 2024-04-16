ads04r/imouto-viewer

commit 30017181e53af8ae371dc9f51d120738a9467d42

Fixed a bit of a photo import issue. Timezones again, who'd have thought it.

viewer/tasks/process.py
@@ -118,7 +118,7 @@ def check_watched_directories():
 		if f.watched_directory.importer == 'jpg':
 			f.import_time = pytz.utc.localize(datetime.datetime.utcnow())
 			f.save(update_fields=['import_time'])
-			photo = import_photo_file(f.path)
+			photo = import_photo_file(f.path, pytz.timezone(settings.TIME_ZONE))
 			if photo.time is None:
 				continue
 			if last_photo is None:

commit 3a5fd7ea25b36fb8ee8cfe89281ce3b98080c514

Fixed a bug that stopped photos importing, introduced in https://github.com/ads04r/imouto-viewer/commit/30017181e53af8ae371dc9f51d120738a9467d42

viewer/tasks/process.py
@@ -1,6 +1,7 @@
 import datetime, pytz, os
 from background_task import background
 from background_task.models import Task
+from django.conf import settings
 
 from viewer.models import WatchedDirectory, Photo
 