d0a712b60ae6f268dabdd525f163494439ed9f7b
Ash <ash@drashsmith.com>
2024-05-19 12:09:26 +0100

    Added some basic docstrings. Closes #164 ... work can continue in #193

--- viewer/models/questionnaire.py
+++ viewer/models/questionnaire.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Questionnaire(models.Model):
+	"""This class represents a questionnaire that may be taken by the user. Results are
+	stored as DataReading objects, and these may be customised in the QuestionnaireAnswer class."""
 	label = models.CharField(max_length=255)
 	description = models.TextField(default='', null=False)
 	intro_text = models.TextField(default='', null=False)
@@ -15,6 +17,13 @@ class Questionnaire(models.Model):
 		if self.random_order:
 			return self.all_questions.order_by('?')
 		return self.all_questions.order_by('ordering')
+	"""
+	One-line command to create a question within the current Questionnaire
+
+	:param question_text: The text to be used in the form of a question, for example "Where are my pants?"
+	:return: The newly created question object
+	:rtype: QuestionnaireQuestion
+	"""
 	def create_question(self, question_text):
 		if self.all_questions.count() == 0:
 			new_id = 1
@@ -23,6 +32,12 @@ class Questionnaire(models.Model):
 		ret = QuestionnaireQuestion(question=question_text, ordering=new_id, questionnaire=self)
 		ret.save()
 		return ret
+	"""
+	Parses a POST data from a Django request and returns a dict of information.
+
+	:return: A dict containing two keys... 'status' is a boolean indicating if the POST data is valid or not, and 'results' contains a dict of values that need to be created as DataReadings by the calling view.
+	:rtype: dict
+	"""
 	def parse_request(self, post_data):
 		ret = {}
 		answered = []
@@ -57,6 +72,15 @@ class QuestionnaireQuestion(models.Model):
 	question = models.TextField()
 	ordering = models.IntegerField()
 	questionnaire = models.ForeignKey(Questionnaire, related_name='all_questions', on_delete=models.CASCADE)
+	"""
+	One-line command to create a possible answer for this question
+
+	:param answer_text: The text to be used as this answer
+	:param reading_key: The type of DataReading that should be created/added to if the user selects this option
+	:param reading_value: The value that should be added to the combined DataReading if the user selects this option. May be negative or zero.
+	:return: The newly created answer object
+	:rtype: QuestionnaireAnswer
+	"""
 	def create_answer(self, answer_text, reading_key, reading_value):
 		if self.all_answers.count() == 0:
 			new_id = 1
