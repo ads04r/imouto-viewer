c293ccbaa86f0adf53aac1efc88dc6a9cbd8f3d5
Ash <ash@drashsmith.com>
2024-06-30 20:44:39 +0100

    Added logging to the manage.py, so now hopefully even uncaught exceptions will be logged to the log file if required.

--- manage.py
+++ manage.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 """Imouto's command-line utility for administrative tasks."""
-import os, sys
+import os, sys, logging
+
+logger = logging.getLogger(__name__)
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imouto.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        sys.stdout.write("Dependencies are not available. Please ensure you have run pip install -r requirements.txt and activated any necessary virtual environment.\n")
-        sys.exit(1)
-    execute_from_command_line(sys.argv)
+	os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imouto.settings')
+	try:
+		from django.core.management import execute_from_command_line
+	except ImportError as exc:
+		sys.stdout.write("Dependencies are not available. Please ensure you have run pip install -r requirements.txt and activated any necessary virtual environment.\n")
+		sys.exit(1)
+	execute_from_command_line(sys.argv)
 
 if __name__ == '__main__':
-    main()
+	try:
+		main()
+	except:
+		logger.exception("Uncaught exception: {0}".format(str(value)))
