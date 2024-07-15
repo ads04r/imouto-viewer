f4e68e9c75e8086db3f67bfb4e177f48b3eaa980
Ash <ash@drashsmith.com>
2024-07-15 18:08:09 +0100

    Added a bit nicer functionality to the admin interface

--- viewer/admin.py
+++ viewer/admin.py
@@ -6,14 +6,50 @@ class PersonAdmin(admin.ModelAdmin):
 	ordering = ['-significant', 'family_name', 'given_name', 'nickname']
 
 class LocationAdmin(admin.ModelAdmin):
-	list_display = ['full_label', 'address', 'exists', 'city', 'country']
+	list_display = ['label', 'address', 'exists', 'city', 'country']
+	ordering = ['full_label']
 
-admin.site.register(Event)
+class MessageAdmin(admin.ModelAdmin):
+	list_display = ['time', 'type', 'person', 'incoming', 'message']
+	ordering = ['-time']
+
+class EventAdmin(admin.ModelAdmin):
+	list_display = ['start_time', 'length_string', 'caption', 'type']
+	ordering = ['-start_time']
+
+class HistoricalEventAdmin(admin.ModelAdmin):
+	list_display = ['date', 'category', 'description']
+	ordering = ['-date']
+
+class LifePeriodAdmin(admin.ModelAdmin):
+	list_display = ['start_time', 'end_time', 'type', 'caption']
+	ordering = ['-start_time']
+
+class DataPointAdmin(admin.ModelAdmin):
+	list_display = ['start_time', 'end_time', 'type', 'value']
+	ordering = ['-start_time']
+
+class WeatherPointAdmin(admin.ModelAdmin):
+	list_display = ['time', 'location', 'description', 'temperature', 'wind_speed', 'humidity', 'visibility', 'wind_direction']
+	ordering = ['-time']
+
+class CountryAdmin(admin.ModelAdmin):
+	list_display = ['label', 'a2']
+	ordering = ['label']
+
+class CityAdmin(admin.ModelAdmin):
+	list_display = ['label', 'country']
+	ordering = ['country', 'label']
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(LifePeriod, LifePeriodAdmin)
+admin.site.register(HistoricalEvent, HistoricalEventAdmin)
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Location, LocationAdmin)
-admin.site.register(DataReading)
-admin.site.register(RemoteInteraction)
-admin.site.register(EventTag)
-admin.site.register(LocationCountry)
-admin.site.register(LocationCity)
+admin.site.register(DataReading, DataPointAdmin)
+admin.site.register(WeatherReading, WeatherPointAdmin)
+admin.site.register(RemoteInteraction, MessageAdmin)
+admin.site.register(LocationCountry, CountryAdmin)
+admin.site.register(LocationCity, CityAdmin)
 
+# TODO: Photo, GitCommit, SchemaOrgClass, WatchedDirectory, WeatherLocation(?)
