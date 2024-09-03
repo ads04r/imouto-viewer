bf4c1ddc45a38685111e2afaf20b65e11cd9294b
Ash <ash@drashsmith.com>
2024-09-03 10:47:56 +0100

    Added pyflakes test

--- viewer/tests/__init__.py
+++ viewer/tests/__init__.py
@@ -2,3 +2,4 @@ from .http import *
 from .models import *
 from .systemd import *
 from .configuration import *
+from .pyflakes import *
--- viewer/tests/pyflakes.py
+++ viewer/tests/pyflakes.py
@@ -1,7 +1,19 @@
 import unittest
 from pyflakes import reporter as modReporter
 from pyflakes.api import checkRecursive
+import sys, os
 
 class PyFlakesTest(unittest.TestCase):
 	def test_details(self):
-		self.assertEqual((2 + 2), 4)
+		base_path = os.path.dirname(os.path.dirname(__file__))
+		functions_path = os.path.join(base_path, 'functions')
+		models_path = os.path.join(base_path, 'models')
+		reporter = modReporter.Reporter(sys.stdout, open(os.devnull, 'w'))
+		files = []
+		for filename in os.listdir(functions_path):
+			if not filename.endswith('.py'):
+				continue
+			files.append(os.path.join(functions_path, filename))
+		ret = checkRecursive(files, reporter)
+		self.assertEqual(ret, 0)
+
