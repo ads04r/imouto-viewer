532d084bdd375a58631b686f89d47683bf405c5f
Ash <ash@drashsmith.com>
2024-09-05 22:39:05 +0100

    Moved from fitparse to fit-tool. Closes #237 and allows for FIT file exporting in the future.

--- viewer/importers/fitfile.py
+++ viewer/importers/fitfile.py
@@ -1,5 +1,5 @@
 import datetime, pytz, math
-from fitparse import FitFile
+from fit_tool.fit_file import FitFile
 
 from viewer.models import Day, DataReading, ImportedFile
 
@@ -15,20 +15,28 @@ def import_fit(parseable_fit_input):
 	data = []
 	earliest_timestamp = None
 	latest_timestamp = None
-	fit = FitFile(parseable_fit_input)
-	for record in fit.get_messages('record'):
+	fit = FitFile.from_file(parseable_fit_input)
+	for record in fit.records:
+		try:
+			name = str(record.message.name)
+		except:
+			name = ''
+		if name != 'record':
+			continue
 		item = {}
-		for recitem in record:
+		for recitem in record.message.fields:
 			k = recitem.name
 			if ((k != 'heart_rate') & (k != 'timestamp') & (k != 'cadence')):
 				continue
 			v = {}
-			v['value'] = recitem.value
-			v['units'] = recitem.units
+			v['value'] = recitem.get_value()
+			v['units'] = recitem.get_units()
 			if ((v['units'] == 'semicircles') & (not(v['value'] is None))):
 				v['value'] = float(v['value']) * ( 180 / math.pow(2, 31) )
 				v['units'] = 'degrees'
 			item[k] = v
+		if item['timestamp']['units'] == 'ms':
+			item['timestamp'] = {'value': datetime.datetime.utcfromtimestamp(float(item['timestamp']['value']) / 1000), 'units': ''}
 		if item['timestamp']['value'].tzinfo is None or item['timestamp']['value'].utcoffset(item['timestamp']['value']) is None:
 			item['timestamp']['value'] = pytz.utc.localize(item['timestamp']['value']) # If no timestamp, we assume UTC
 		if earliest_timestamp is None:
@@ -44,6 +52,8 @@ def import_fit(parseable_fit_input):
 		if 'heart_rate' in item:
 			newitem['heart'] = item['heart_rate']['value']
 		if 'cadence' in item:
+			if item['cadence']['value'] is None:
+				item['cadence']['value'] = 0
 			if item['cadence']['value'] > 0:
 				newitem['cadence'] = item['cadence']['value']
 		newitem['length'] = 1
