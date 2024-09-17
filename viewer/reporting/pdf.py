806acc619943db3913343de8d44a08a8997050e7
Ash <ash@drashsmith.com>
2024-09-17 23:31:37 +0100

    It's now possible to use a custom style when generating report PDFs

--- viewer/reporting/pdf.py
+++ viewer/reporting/pdf.py
@@ -3,7 +3,7 @@ from viewer.reporting.styles.basic import ImoutoSampleReportStyle
 import logging
 logger = logging.getLogger(__name__)
 
-def generate_year_pdf(year, export_file):
+def generate_year_pdf(year, export_file, document_class=ImoutoSampleReportStyle):
 
-	report = ImoutoSampleReportStyle(year)
+	report = document_class(year)
 	report.generate(export_file)
