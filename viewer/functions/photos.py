54039390b854a39dc31adace1a4c6c3ec1973325
Ash <ash@drashsmith.com>
2024-09-17 22:49:02 +0100

    Added a function to generate a hexagonal thumbnail, rather than a boring old square one

--- viewer/functions/photos.py
+++ viewer/functions/photos.py
@@ -1,9 +1,30 @@
 import datetime, pytz
 from viewer.models import Photo, Event, Location
 from viewer.functions.locations import nearest_location
+from PIL import Image, ImageDraw
+from math import sin, cos, pi
 
 import logging
 logger = logging.getLogger(__name__)
+
+def hexagon_crop(im, landscape=True):
+
+	w = float(im.width) / 2
+	h = float(im.height) / 2
+	shape = ()
+	for i in range(0, 6):
+		if landscape:
+			t = (float(i) + 0.5) * (pi / 3.0)
+		else:
+			t = float(i) * (pi / 3.0)
+		shape = shape + (((w + (sin(t) * w), h + (cos(t) * h))))
+
+	mask = Image.new('RGBA', im.size)
+	d = ImageDraw.Draw(mask)
+	d.polygon(shape, fill='#000')
+	ret = Image.new('RGBA', im.size)
+	ret.paste(im, (0, 0), mask)
+	return ret
 
 def locate_photos_by_exif(since=None, reassign=False):
 
