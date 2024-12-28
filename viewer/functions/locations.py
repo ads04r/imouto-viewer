360caad862ad62e7153cfd9d2b18d93ad6464abb
Ash <ash@drashsmith.com>
2024-12-28 17:19:23 +0000

    Changed the way location event creation works in order to reduce the number of duplicates

--- viewer/functions/locations.py
+++ viewer/functions/locations.py
@@ -100,7 +100,7 @@ def create_location_events(minlength=300):
 		home_id = settings.USER_HOME_LOCATION
 	except:
 		home_id = -1
-	dts = Event.objects.filter(type='loc_prox').order_by('-start_time')[0].start_time
+	dts = Event.objects.filter(type='loc_prox').exclude(location__id=home_id).order_by('-start_time')[0].start_time
 	dte = pytz.utc.localize(datetime.datetime.utcnow()).replace(hour=0, minute=0, second=0) + datetime.timedelta(days=1)
 	ret = []
 	while dts < dte:
@@ -140,6 +140,15 @@ def create_location_events(minlength=300):
 				continue
 			if dur < minlength:
 				logger.debug("            event is less than " + str(minlength) + " seconds, skipping")
+				continue
+			if Event.objects.filter(start_time__gte=start_time, start_time__lte=start_time).count() > 0:
+				logger.debug("            existing event(s) intersect, skipping")
+				continue
+			if Event.objects.filter(end_time__gte=start_time, end_time__lte=start_time).count() > 0:
+				logger.debug("            existing event(s) intersect, skipping")
+				continue
+			if Event.objects.filter(start_time__lte=end_time, end_time__gte=start_time).count() > 0:
+				logger.debug("            existing event(s) intersect, skipping")
 				continue
 
 			caption = event_label(start_time, end_time)
